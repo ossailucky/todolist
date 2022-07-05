112307a4ebef91869308ca9afee05e6511c25a9e
Ossai Lucky <luckyblaqy@gmail.com>
2022-07-06 00:21:13 +0100

    commiting to do list v1

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express();
+
+const listItem = ["Buy Food", "Cook Food", "Eat Food"]
+const workItems = [];
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+
+  let today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {listTitle: day, newListItems: listItem});
+})
+
+
+app.post("/", (request,response)=>{
+
+  let item = request.body.newItem;
+console.log(request.body)
+  if (request.body.list === "Work"){
+    workItems.push(item)
+    response.redirect("/work")
+  }else{
+    listItem.push(item)
+    response.redirect("/")
+  }
+  
+  
+})
+
+app.get("/work",(req,res)=>{
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+
+
+
+app.listen(3000,()=>{
+  console.log("server started listening on port 3000")
+})
